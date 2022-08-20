c86d1037512710084e9291a472a1cf19ec006ec4
Berwyn Tan <berwyntan@gmail.com>
2022-08-21 00:31:32 +0800

    try API and do wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
-
+const remove = ["and", "the", "is", "a", "on", "of", "under", "over", "or"]
 function App() {
   const [count, setCount] = useState(0)
+  const callAPI = () => {
+    fetch("https://api.nasa.gov/planetary/apod?api_key=CmaRrOqD96tV80CDIrjTmpawIrei2fv7hBEgOqH8&count=10")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    });
+  }
+
+  useEffect(() => {callAPI()}, [])
 
   return (
     <div className="App">
-      testing
+        Astronomy
     </div>
   )
 }
