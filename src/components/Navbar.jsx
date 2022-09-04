6954be43d395a9d499b79112701bb59ce158ae8c
Berwyn Tan <berwyntan@gmail.com>
2022-09-04 14:15:26 +0800

    fix: icons not rendering in vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import caretDown from "../icons/caret-down.svg"
+import clock from "../icons/clock.svg"
+
 export default function Navbar() {
     return (
         <div className="navbar bg-slate-900 text-slate-50 fixed top-0">
@@ -6,7 +9,7 @@ export default function Navbar() {
                 <div class="dropdown hover:bg-slate-900/25">
                     <div className="flex items-center">
                         <label tabindex="0" class="btn btn-ghost rounded-btn">Interstellar   
-                        <img class="h-5 ml-4 invert" src="../src/icons/caret-down.svg" />
+                        <img class="h-5 ml-4 invert" src={caretDown} />
                         </label>
                     </div>
                     <ul tabindex="0" class="menu dropdown-content p-1 shadow bg-slate-900 text-slate-50 rounded-box w-52 mt-1">
@@ -20,7 +23,7 @@ export default function Navbar() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                 </button> */}
                 <button className="btn btn-square btn-ghost invert">
-                    <img className="h-5" src="./src/icons/clock.svg" />
+                    <img className="h-5" src={clock} />
                 </button>
                 <button className="btn btn-square btn-ghost invert">
                     <img className="h-5" src="./src/icons/shuffle.svg" />
